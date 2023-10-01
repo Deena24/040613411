c4ca784d6532acdf5b164a72d0643b84b52b6dd9
Deena24 <s6404062630341@email.kmutnb.ac.th>
2023-10-01 23:05:01 +0700

    lab9 commit

---
+++ Lab9DBConnection/workshop7.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <form action="insert-member7.php" method="post">
+        username : <input type="text" name="username"><br>
+        password : <input type="password" name="password"><br>
+        name: <input type="text" name="name"><br>
+        address: <br><textarea name="address" rows="3" cols="40"></textarea><br>
+        mobile: <input type="tel" name="mobile"><br>
+        email: <input type="email" name="email"><br>
+        รูปภาพ: <input type="file" name="image"><br>
+        <input type="submit" value="เพิ่มสมาชิก ">
+    </form>
+</body>
+</html>
